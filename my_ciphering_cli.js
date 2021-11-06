c651d27e475de227052046c2e590483bac29467b
Antonina Dubich <dubich.t@gmail.com>
2021-11-06 13:59:55 +0300

    fix: error output

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -4,29 +4,46 @@ const path = require("path");
 const TransformStream = require("./streams/chipherTransform");
 const stream = require("stream");
 const util = require("util");
+const {ConfigError, DuplicateFlagError, InputError, OutputError} = require('./modules/errors')
 
 const data = helper.getData(process.argv);
-
-if (!data.get("-c") || !helper.validateConfig(data.get("-c"))) {
-  helper.error("You have problem with config");
-}
-
-if (!helper.validateFlags(process.argv.slice(2))) {
-  helper.error("Flags should not be repeated");
-}
-
 const inputStream = data.has("-i")
-  ? fs.createReadStream(path.resolve(__dirname, data.get("-i")), "utf8")
-  : process.stdin;
+    ? fs.createReadStream(path.resolve(__dirname, data.get("-i")), "utf8")
+    : process.stdin;
 const outputStream = data.has("-o")
-  ? fs.createWriteStream(path.resolve(__dirname, data.get("-o")))
-  : process.stdout;
+    ? fs.createWriteStream(path.resolve(__dirname, data.get("-o")))
+    : process.stdout;
 const transformStream = new TransformStream(data.get("-c"));
-
 const pipeline = util.promisify(stream.pipeline);
 
+try {
+  if (!data.get("-c") || !helper.validateConfig(data.get("-c"))) {
+    throw new ConfigError("You have problem with config");
+  }
+
+  if (!helper.validateFlags(process.argv.slice(2))) {
+    throw new DuplicateFlagError("Flags should not be repeated");
+  }
+}catch (err){
+  helper.error(`${err.name}: ${err.message}`)
+}
+
 inputStream.on("error", () => {
-  helper.error("You have problem in input file");
+  try {
+    throw new InputError("You have problem in input file");
+  } catch (err){
+    helper.error(`${err.name}: ${err.message}`)
+  }
 });
 
+outputStream.on("error", () => {
+  try {
+    throw new OutputError("You have problem in output file");
+  } catch (err){
+    helper.error(`${err.name}: ${err.message}`)
+  }
+});
+
+
 pipeline(inputStream, transformStream, outputStream);
+
