5ab8c38dd7d9b93bafa8cf3c909795eca75c67f3
Antonina Dubich <dubich.t@gmail.com>
2021-11-18 12:24:30 +0300

    fix: problem with config  error

--- ciphering_cli.js
+++ ciphering_cli.js
@@ -11,14 +11,6 @@ const ReadStream = require('./streams/ReadableStream');
 const transformFunction = require('./streams/TransformStream');
 
 const data = helper.getData(process.argv);
-const inputStream = data.has("-i")
-  ? new ReadStream(path.resolve(__dirname, data.get("-i")), "utf8")
-  : process.stdin;
-const outputStream = data.has("-o")
-  ? new WritableStream(path.resolve(__dirname, data.get("-o")))
-  : process.stdout;
-const transformStream = transformFunction(data.get("-c"));
-const pipeline = util.promisify(stream.pipeline);
 
 try {
   if (!data.get("-c") || !helper.validateConfig(data.get("-c"))) {
@@ -31,6 +23,15 @@ try {
 } catch (err) {
   helper.error(`${err.name}: ${err.message}`);
 }
+
+const inputStream = data.has("-i")
+  ? new ReadStream(path.resolve(__dirname, data.get("-i")), "utf8")
+  : process.stdin;
+const outputStream = data.has("-o")
+  ? new WritableStream(path.resolve(__dirname, data.get("-o")))
+  : process.stdout;
+const transformStream = transformFunction(data.get("-c"));
+const pipeline = util.promisify(stream.pipeline);
 
 pipeline(
     inputStream,
